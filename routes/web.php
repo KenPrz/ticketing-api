6290eb875c6272c2be7a580c66edc35789fc3757
Kenprz <kendarylausteroperez@gmail.com>
2025-01-29 00:05:09 +0800

    Update welcome message in the root route to greet users

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['Message' => 'Hello Human, Welcome to Q-phoria!'];
 });
 
 require __DIR__.'/auth.php';
