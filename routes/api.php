edf006a32f076ad79f532c8562c6cb80d9904290
Kenprz <kendarylausteroperez@gmail.com>
2025-01-29 00:01:38 +0800

    Add a test route that returns a JSON response with a status message

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function () {
+    return response()->json(['message' => 'Hello World!', 'status' => 'Alive', 'Goal' => 'Make something amazing!']);
+});
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
