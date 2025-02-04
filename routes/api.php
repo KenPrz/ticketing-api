2a67fd8f5eb6620f2f50c322559e7374a7bd9ae0
Kenprz <kendarylausteroperez@gmail.com>
2025-02-05 00:31:47 +0800

    Add HomeController and update API routes for home endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Common\AuthController;
+use App\Http\Controllers\Common\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,17 +9,10 @@ Route::get('/test', function () {
     return response()->json(['message' => 'Hello World!', 'status' => 'Alive', 'Goal' => 'Make something amazing!']);
 });
 
-Route::post('/login', [AuthController::class, 'login'])
-    ->name('login')
-    ->middleware('guest');
-
-Route::post('/register', [AuthController::class, 'register'])
-    ->name('register')
-    ->middleware('guest');
-
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])
-        ->name('logout');
-    Route::get('/user', [AuthController::class, 'user'])
-        ->name('user');
+
+    Route::get('/home', [HomeController::class, 'index'])
+        ->name('home');
 });
+
+require __DIR__.'/auth-api.php';
